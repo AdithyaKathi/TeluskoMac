cc0d9d55895faec884508e355eed02b218239b54
AdithyaKathi <113934881+AdithyaKathi@users.noreply.github.com>
2023-05-20 19:27:00 +0530

    Update Pascal.java

--- Pascal.java
+++ Pascal.java
@@ -22,11 +22,43 @@ public class Pascal
         }
         return res;
     }
+	
+    // It Computes Pascal Triangle In Iterative Approach	
+    public static void iterPascal(int num){
+          for (int i = 0; i < num; i++) {
+                int number = 1;
+                System.out.printf("%" + (num - i) * 2 + "s", ""); 
+		// Printing spaces for alignment
+
+                for (int j = 0; j <= i; j++){
+                    System.out.printf("%4d", number);
+                    number = number * (i - j) / (j + 1);
+                }
+                System.out.println();
+		// This Is The Best Of 3 Approaches That I've Tried
+           }
+     }
+     
+     // It Computes Pascal Triangle In Recursive Approach
+     public static int calculatePascalValue(int row, int col){
+        if(col == 0 || col == row){
+            return 1;
+        }else{
+            return calculatePascalValue(row - 1,col - 1) + calculatePascalValue(row - 1,col);
+        }
+	     // S H O R T
+	     // S I M P L E
+	     // Y E T    I N S I G H T F U L
+     }
+	
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter The Number : ");
 		// User Input
 		int num = scan.nextInt();
+		System.out.println("The Memoized Output : ");
+		System.out.println();
 		System.out.println("The Pascals Triangle is : ");
 		List<List<Integer>> res = memoizedPascal(num);
 		//  Printing Pascals Triangle
@@ -34,5 +66,20 @@ public class Pascal
 		    System.out.println(res.get(i).toString());
 		    System.out.println();
 		}
+		
+		System.out.println("The Iterative Output : ");
+		System.out.println();
+		iterPascal(num);
+		System.out.println();
+		System.out.println();
+		System.out.println("The Recursive Approach : ");
+		
+		System.out.println("The Pascal's Triangle ");
+		for (int i = 0; i <num; i++) {
+                     for (int j = 0; j <= i; j++) {
+                            System.out.print(calculatePascalValue(i, j) + " ");
+                     }
+                     System.out.println();
+                }
 	}
 }
